6fb5695006587659344f60731f8bfe5c309d5e0b
Liu Zhenchang <liuzhenchangxc@gmail.com>
2016-04-30 18:26:59 +0800

    add IService and Service

---
+++ Iss.LiveClassRoom.ServiceLayer/Services/VideoService.cs
@@ -0,0 +1,18 @@
+﻿using Iss.LiveClassRoom.Core.Models;
+using Iss.LiveClassRoom.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Iss.LiveClassRoom.Core.Repositories;
+
+namespace Iss.LiveClassRoom.ServiceLayer.Services
+{
+    public class VideoService : Service<Video>, IVideoService
+    {
+        public VideoService(IUnitOfWork uow) : base(uow)
+        {
+        }
+    }
+}
