657e4e0b853555dddbbf7418f03eed7007ec5f47
Harsha <harshavardhinimoorthy@gmail.com>
2016-05-01 18:44:20 +0800

    Create view models

---
+++ Iss.LiveClassRoom.FrontEnd/Models/QuizViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Iss.LiveClassRoom.FrontEnd.Models
+{
+    public class QuizViewModel
+    {
+
+        [Required]
+        public string Id { get; set; }
+        [Required]
+        public string Question { get; set; }
+
+        [Required]
+        public string CourseId { get; set; }
+
+    }
+
+
+    public static class QuizExtension
+    {
+        public static Quiz ToDomainModel(this QuizViewModel model)
+        {
+            return new Quiz()
+            {
+                Id = model.Id,
+                Question = model.Question,
+                CourseId = model.CourseId,
+
+            };
+        }
+
+        public static QuizViewModel ToViewModel(this Quiz model)
+        {
+            return new QuizViewModel()
+            {
+                Id = model.Id,
+                Question = model.Question,
+                CourseId = model.CourseId,
+
+
+            };
+        }
+    }
+}
+
+    
+
---
+++ Iss.LiveClassRoom.FrontEnd/Models/StudentViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Iss.LiveClassRoom.FrontEnd.Models
+{
+    public class StudentViewModel
+    {
+
+
+        [Required]
+        public string Id { get; set; }
+    }
+    public static class StudentExtension
+    {
+        public static Student ToDomainModel(this StudentViewModel model)
+        {
+            return new Student()
+            {
+                Id = model.Id,
+                
+            };
+        }
+
+        public static StudentViewModel ToViewModel(this Student model)
+        {
+            return new StudentViewModel()
+            {
+                Id = model.Id,
+               
+            };
+        }
+    }
+}
